3fec0d363a299ff6569890818d1af14acb89ded1
Akshat-Dave05 <akshattdave05@gmail.com>
2025-09-08 19:25:22 +0530

    Add files via upload

---
+++ 35.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& nums, int target){
+    map<int, int> m;
+    for(int i = 0; i < nums.size(); i++){
+        m.insert({i, nums[i]});
+        
+    }    
+}
+int main(){
+
+    return 0;
+}
